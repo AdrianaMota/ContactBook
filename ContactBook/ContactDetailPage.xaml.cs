d7ef7ffc51de0532283cf7d98a8b3ad72e22f79c
adrianamota <lishamota@hotmail.com>
2019-08-24 00:44:37 -0400

    First Commit

---
+++ ContactBook/ContactDetailPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ContactBook
+{
+    public partial class ContactDetailPage : ContentPage
+    {
+        public ContactDetailPage(int contId)
+        {
+            InitializeComponent();
+        }
+        public async void OnButtonClicked(object sender, EventArgs args)
+        {
+            if (firstName.Text == null || lastName.Text == null)
+            {
+                var alert = await DisplayAlert("WAIT!", "Please enter the name or", "Ok", "Go back");
+                if (alert == false)
+                {
+                    await Navigation.PopAsync();
+                }
+            }
+
+        }
+    }
+}
---
+++ ContactBook/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ContactBook.Models
+{
+    public class Contact : ContentPage
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Name { get {
+                return String.Format("{0} {1}", FirstName, LastName);
+            } }
+
+
+    }
+}
+
